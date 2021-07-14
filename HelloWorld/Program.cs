ab1047d9ab9cdf111f55e05a8b99231f4da113b9
Greg Doud <gpdoud@gmail.com>
2021-07-14 11:27:23 -0400

    Committed by GitUtil

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,9 +1,64 @@
 ﻿using System;
 
-namespace HelloWorld {
+using HelloWorld;
+
+namespace TQL.Bootcamp.Class1 {
     class Program {
+        
+        readonly string str;
+
         static void Main(string[] args) {
-            Console.WriteLine("Hello World in C#!");
+
+            var sarah = new Student();
+            sarah.Firstname = "Sarah";
+            sarah.Lastname = "George";
+            sarah.SetHireDate(2017, 6, 26);
+
+            var charlie = new Student();
+            charlie.Firstname = "Charlie";
+            charlie.Lastname = "Traylor";
+
+            var mattea = new Student();
+            mattea.Firstname = "Mattea";
+            mattea.Lastname = "Swain";
+
+            sarah.Print();
+            charlie.Print();
+            mattea.Print();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //int? i = null;
+            //string s = null;
+
+            //var company = "TQL";
+            //var student = "Greg";
+            //var years = 2;
+            //years = 3;
+            //var yesNo = true;
+            //{
+            //    var x = 0;
+            //    Console.WriteLine($"x is {x}");
+            //    years = 4;
+            //}
+            //Console.WriteLine($"Hello World from {student} in C# from {company}!");
+            //Console.WriteLine($"My name is {student} and I've been at {company} for {years} years!");
         }
     }
 }
---
+++ HelloWorld/Student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld {
+    class Student {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public bool Active { get; set; }
+        private DateTime HireDate { get; set; }
+
+        public void SetHireDate(int year, int month, int day) {
+            if(year < 1950 || year > 2200) {
+                Console.WriteLine($"Year must be between 1950 and 2200");
+                return;
+            }
+            if(month < 1 || month > 12) {
+                Console.WriteLine($"Do you know what a calendar is???? 1 <= x <= 12");
+                return;
+            }
+            if(day < 1 || day > 31) {
+                Console.WriteLine($"Invalid day!");
+                return;
+            }
+            HireDate = new DateTime(year, month, day);
+        }
+        public void Print() {
+            Console.WriteLine($"{Firstname} {Lastname} Hired: {HireDate.ToString("MM/dd/yyyy")}");
+        }
+    }
+}
